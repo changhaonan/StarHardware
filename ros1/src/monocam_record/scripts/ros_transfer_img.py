014ec28286d63a734c562bbcf20548ca5116e537
changhaonan <chnme40cs@gmail.com>
2022-09-09 23:17:08 -0400

    update: further improve the scripts

--- ros1/src/monocam_record/scripts/ros_transfer_img.py
+++ ros1/src/monocam_record/scripts/ros_transfer_img.py
@@ -1,8 +1,18 @@
 import os
 
 if __name__ == "__main__":
+    # 1. Prepare the path
     bag_file = "/home/robot-learning/Projects/StarHardware/ros1/src/monocam_record/data/realsense2_2022-09-09-22-44-25.bag"
     color_topic_name = "/rgb_map"
     depth_topic_name = "/depth_map"
-    save_dir = "."
+    save_dir = "./"
+
+    # 2. Create the directory
+    if not os.path.exists(os.path.join(save_dir, "cam-00")):
+        os.makedirs(os.path.join(save_dir, "cam-00"))
+    
+    # 3. Transfer the data
     os.system(f"rosrun monocam_record ros2img --bag_file {bag_file} --color_topic_name {color_topic_name} --depth_topic_name {depth_topic_name} --save_dir {save_dir}")
+
+    # 4. Log
+    print(f"Transfer the data from {bag_file} to {save_dir} successfully!")
